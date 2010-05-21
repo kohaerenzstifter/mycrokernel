80522625411ba14d913af3d6861fdd6f43658f8e
martin.knappe <martin.knappe@35a491bd-9a6e-23b8-3296-c9b92dd79895>
2010-05-21 19:44:49 +0000

    some rework

--- usrlib/syscall.h
+++ usrlib/syscall.h
@@ -7,5 +7,16 @@ uint32_t call_syscall_receive(tss_t *receiver, void *where, uint32_t length, err
 //tss_t *call_syscall_requestFeature(uint32_t feature);
 //void call_syscall_send(tss_t *receiver, void *where, uint32_t length);
 uint32_t call_syscall_send_by_feature(uint32_t feature,void *where,uint32_t length, boolean_t block, err_t *error);
-//int call_syscall_show_tss();
+void call_syscall_exit();
+
+
+
+
+#define terror(x) \
+  x; \
+  if (error != OK) { \
+    goto finish; \
+  }
+
+
 #endif
--- video/main.c
+++ video/main.c
@@ -2,7 +2,7 @@
 #include "../include/const.h"
 #include "../usrlib/syscall.h"
 #include "prototype.h"
-
+/*
 int main()
 {
   err_t error = OK;
@@ -19,4 +19,19 @@ int main()
     putstring(buffer);
     putchar(LF);
   }
+}*/
+
+int main()
+{
+  err_t error = OK;
+//  terror(call_syscall_request_irq(VIDEO, &error))
+  terror(call_syscall_setFeature(FEATURE_SPEAK, &error))
+
+
+
+finish:
+/*  if (error) {
+    putstring("error: " error_to_string(error)); putchar(LF);
+  }*/
+  call_syscall_exit();
 }
