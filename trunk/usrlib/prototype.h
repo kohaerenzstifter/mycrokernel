e22a54973a73529a7fe3c04d96a801ec45815ce3
martin.knappe <martin.knappe@35a491bd-9a6e-23b8-3296-c9b92dd79895>
2010-10-03 18:15:36 +0000

    identifying hd

--- trunk/usrlib/prototype.h
+++ trunk/usrlib/prototype.h
@@ -1,10 +1,10 @@
-#ifndef USRLIB_SYSCALL_H
-#define USRLIB_SYSCALL_H
+#ifndef USRLIB_PROTOTYPE_H
+#define USRLIB_PROTOTYPE_H
 
 void outf(err_t *error, boolean_t block, char *fmt, ...) __attribute__((format (printf,3, 4)));
 uint32_t strlen(char *me);
-char *err2String(int errnum);
-void strncpy(char *dst, char *dst, int num);
+char *err2String(err_t errnum);
+void strncpy(char *dst, char *src, int num);
 boolean_t streq(char *a, char *b);
 
 #endif
